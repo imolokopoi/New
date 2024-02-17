b43e572b8863a7ac535a3a55f9cdb2ac667632bc
imolokopoi <iryna.molokopoi@gmail.com>
2024-02-17 10:45:41 -0600

    last one

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@
 //Напишіть функцію яка ітеративно (в циклі) буде знаходити суму 
   //всіх непарних додатніх чисел до якогось числа.
  
-  function iterativeOddSumTo(number) {
-    let oddSumm=0;
-for (let i=1; i<=number; i+=2){
-    oddSumm+=i;
+  //function iterativeOddSumTo(number) {
+    //let oddSumm=0;
+//for (let i=1; i<=number; i+=2){
+  //  oddSumm+=i;
 
-};
-return oddSumm;
-};
-    console.log(iterativeOddSumTo(1)) // 1
-    console.log(iterativeOddSumTo(9)) // 25
-    console.log(iterativeOddSumTo(10)) // 25
+//};
+//return oddSumm;
+//};
+//    console.log(iterativeOddSumTo(1)) // 1
+//    console.log(iterativeOddSumTo(9)) // 25
+//    console.log(iterativeOddSumTo(10)) // 25
 
    // Напишіть функцію яка рекурсивно буде знаходити суму всіх непарних додатніх чисел до якогось числа.  
-    function recursiveOddSumTo(number) {
-        if (number<=0){
-        return 0;}
-        else if (number%2===0){
-            return recursiveOddSumTo(number-1);
-        }
-        else {return number+recursiveOddSumTo(number-2);}
-    };
-        console.log(recursiveOddSumTo(1)) // 1
-        console.log(recursiveOddSumTo(9)) // 25
-        console.log(recursiveOddSumTo(10)) // 25   
+//    function recursiveOddSumTo(number) {
+//        if (number<=1){
+//        return 0;}
+//        else if (number%2===0){
+//            return recursiveOddSumTo(number-1);
+//        }
+//        else {return number+recursiveOddSumTo(number-2);}
+//    };
+//        console.log(recursiveOddSumTo(1)) // 1
+//        console.log(recursiveOddSumTo(9)) // 25
+//        console.log(recursiveOddSumTo(10)) // 25   
 
 //
 //Напишіть стрілочну функцію isXOEqual яка перевіряє чи рядок має однакову кількість символів «x» та «o». 
@@ -33,20 +33,20 @@ return oddSumm;
 //символи.
 
 
-const isXOEqual = (str) => {
-    let sumo=0;
-    let sumx=0;
+/*const isXOEqual = (str) => {
+    let sumO=0;
+    let sumX=0;
     str=str.toLowerCase();
     console.log(str)
     for (let it=0; it<str.length; it++){
     if (str[it]==="o"){
-            sumo++;
+            sumO++;
         }
     else if (str[it]==="x"){
-            sumx++;
+            sumX++;
     }
 }
-return sumo===sumx;
+return sumO===sumX;
 }
 
 console.log(isXOEqual("ooxx")) // true
@@ -54,3 +54,31 @@ console.log(isXOEqual("xooxx")) // false
 console.log(isXOEqual("ooxXm")) // true
 console.log(isXOEqual("zpzpzpp")) // коли немає 'x' та 'o' то має повертати true бо 0 = 0
 console.log(isXOEqual("zzoo")) // false
+
+//"Просунута робота з масивами"
+// Задача 1
+//const resultsArray=[1, 2, [3, [4]]];
+//let resultsOfArray=resultsArray.flat(Infinity);
+//const productOfArray=resultsOfArray.reduce((accumulator, currentValue)=>accumulator * currentValue, 1);
+//console.log(resultsOfArray); 
+//console.log(productOfArray); // 24
+*/
+//Задача 2
+// приклад об'єкту
+const priceData = {
+    Apples: '23.4',
+    BANANAS: '48',
+    oRAngGEs: '48.7584',
+    };
+    
+    function optimizer(data) {
+        const newData=priceData((accumulator,priceData)=>{
+
+        }, {})
+    };
+    
+    let updatedPriceData = optimizer(priceData);
+    
+    console.log(updatedPriceData) // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
+    
+    
